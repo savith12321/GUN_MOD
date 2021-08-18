43c5ce1f9d15b05126a7d0c719477d91ea03d9d0
savith nishitha <isavith.nishitha@gmail.com>
2021-08-18 10:53:22 +0530

    cleaning up the main file

---
+++ src/main/java/com/gun/mod/GunMod.java
@@ -0,0 +1,30 @@
+package com.gun.mod;
+
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("gn")
+public class GunMod
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public GunMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+
+}
